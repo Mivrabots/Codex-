97abae7a79416e9890a6e53da53b578c5bccfd94
zhayne12 <156608502+zhayne12@users.noreply.github.com>
2024-03-27 16:45:18 -0500

    hello.py

--- cogs/hello.py
+++ cogs/hello.py
@@ -1,1 +1,15 @@
+import discord 
+from discord.ext import commands
+
+class Hello(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def hello(self, ctx):
+        await ctx.send(f'Hello, {ctx.author.mention}!')
+
+def setup(bot):
+    bot.add_cog(Hello(bot))
+
 
